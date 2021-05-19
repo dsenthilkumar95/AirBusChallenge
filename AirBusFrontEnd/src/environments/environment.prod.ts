9538267870de737f8cf7a858e837a4e7ab6d8c9c
KumarDas, Senthil <Senthil_KumarDas@comcast.com>
2021-05-19 18:15:32 +0530

    Another Ec2 Instance

--- AirBusFrontEnd/src/environments/environment.prod.ts
+++ AirBusFrontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-3-143-252-74.us-east-2.compute.amazonaws.com:8080'
+  apiUrl: 'http://ec2-3-15-223-68.us-east-2.compute.amazonaws.com:8080'
 };
